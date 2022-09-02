ca4c6705d05b0317d872825e399bd2358ce38575
Bennett Smrdel <bennett.smrdel@heysparkbox.com>
2022-09-02 10:47:53 -0500

    fix: fixed merge problem
    - Re-added token form body instead of the weird merge conflict that
    happened.
    
    FSA22V2-108

--- src/scripts/creatures.js
+++ src/scripts/creatures.js
@@ -120,142 +120,52 @@ async function getCustomCreaturesData() {
     }
 }
 
-function toggleNewCreatureForm() {
-    creatureFormOpen = !creatureFormOpen;
-    if (creatureFormOpen) {
-        const window = document.querySelector('body').appendChild(document.createElement('div'));
-        window.classList.add('creatures-window-form');
-        window.insertAdjacentHTML('beforeend', `
-            <div class="creatures-content">
-                <div class="creatures-window-form__header">
-                    <h2>New Creature</h2>
-                </div>
-                <form class="creatures-window-form__body" onsubmit="submitCreatureForm(event)">
-                    <label>Token
-                        <input type="file">
-                    </label>
-                    <div class="creatures-window-form__body--box">
-                        <label>Name
-                            <input required onchange="creatureFormName = event.target.value">
-                        </label>
-                        <label>Size
-                            <select onchange="creatureFormSize = event.target.value">
-                                <option value="tiny">Tiny</option>
-                                <option value="small">Small</option>
-                                <option value="medium" selected>Medium</option>
-                                <option value="large">Large</option>
-                                <option value="huge">Huge</option>
-                                <option value="gargantuan">Gargantuan</option>
-                            </select>
-                        </label>
-                        <label>Type
-                            <input class="input--md" onchange="creatureFormType = event.target.value">
-                        </label>
-                        <label>Alignment
-                            <input class="input--sm" onchange="creatureFormAlignment = event.target.value">
-                        </label>
-                        <label>AC
-                            <input class="input--sm" type="number" onchange="creatureFormAc = event.target.value" required>
-                        </label>
-                        <label>Hit Points
-                            <input class="input--sm" type="number" onchange="creatureFormHitPoints = event.target.value" required>
-                        </label>
-                        <label>Hit Dice
-                            <input class="input--sm" onchange="creatureFormHitDice = event.target.value">
-                        </label>
-                    </div>
-                    <div class="creatures-window-form__body--box">
-                        <div>
-                            <div class="form__input-add form__input-add--speed">
-                                <label>Movement
-                                    <div class="flex-container">
-                                        <p>Walk</p>
-                                        <input placeholder="30" type="number" class="input--sm creature-inputs__speed-value">
-                                    </div>
-                                    <div class="flex-container">
-                                        <p>Swim</p>
-                                        <input placeholder="30" type="number" class="input--sm creature-inputs__speed-value">
-                                    </div>
-                                    <div class="flex-container">
-                                        <p>Burrow</p>
-                                        <input placeholder="30" type="number" class="input--sm creature-inputs__speed-value">
-                                    </div>
-                                    <div class="flex-container">
-                                        <p>Fly</p>
-                                        <input placeholder="30" type="number" class="input--sm creature-inputs__speed-value">
-                                    </div>
-                                    <div class="flex-container">
-                                        <p>Climb</p>
-                                        <input placeholder="30" type="number" class="input--sm creature-inputs__speed-value">
-                                    </div>
-                                </label>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="creatures-window-form__body--box">
-                        <label>Str
-                            <input class="input--sm" type="number" onchange="creatureFormStr = event.target.value" value="0" required>
-                        </label>
-                        <label>Dex
-                            <input class="input--sm" type="number" onchange="creatureFormDex = event.target.value" value="0" required>
-                        </label>
-                        <label>Con
-                            <input class="input--sm" type="number" onchange="creatureFormCon = event.target.value" value="0" required>
-                        </label>
-                        <label>Int
-                            <input class="input--sm" type="number" onchange="creatureFormInt = event.target.value" value="0" required>
-                        </label>
-                        <label>Wis
-                            <input class="input--sm" type="number" onchange="creatureFormWis = event.target.value" value="0" required>
-                        </label>
-                        <label>Char
-                            <input class="input--sm" type="number" onchange="creatureFormChar = event.target.value" value="0" required>
-                        </label>
-                    </div>
-                    <div class="creatures-window-form__body--box">
-                        <div>
-                            <div class="form__input-add form__input-add--proficiency">
-                                <label>Proficiencies
-                                    <div class="flex-container">
-                                        <input placeholder="Perception" class="input--md creature-inputs__proficiency-name">
-                                        <input placeholder="6" type="number" class="input--sm creature-inputs__proficiency-value">
-                                    </div>
-                                </label>
-                            </div>
-                            <button type="button" onclick="addInputs('proficiency')" class="creature-form__btn--input">Add proficiency</button>
-                        </div>
-                    </div>
-                    <div class="creatures-window-form__body--box">
-                        <label>Vulnerabilities
-                            <textarea rows="3" cols="40" placeholder="fire, thunder" onchange="creatureFormVul = event.target.value"></textarea>
-                        </label>
-                    </div>
-                    <div class="creatures-window-form__body--box">
-                        <label>Resistances
-                            <textarea rows="3" cols="40" placeholder="poison, bludgeoning" onchange="creatureFormRes = event.target.value"></textarea>
-                        </label>
-                    </div>
-                    <div class="creatures-window-form__body--box">
-                        <label>Damage Immunities
-                            <textarea rows="3" cols="40" placeholder="nonmagical slashing" onchange="creatureFormDmgImmune = event.target.value"></textarea>
-                        </label>
-                    </div>
-                    <div class="creatures-window-form__body--box">
-                        <label>Condition Immunities
-                            <textarea rows="3" cols="40" placeholder="prone, restrained" onchange="creatureFormConImmune = event.target.value"></textarea>
-                        </label>
-                    </div>
-                    <div class="creatures-window-form__body--box">
-                        <div>
-                            <div class="form__input-add form__input-add--sense">
-                                <label>Senses
-                                    <div class="flex-container">
-                                        <input placeholder="Darkvision" class="input--md creature-inputs__sense-name">
-                                        <input placeholder="60" type="number" class="input--sm creature-inputs__sense-value">
-                                    </div>
-                                </label>
-                            </div>
-                            <button type="button" onclick="addInputs('sense')" class="creature-form__btn--input">Add sense</button>
+const creatureFormBody = `
+<div class="creatures-content">
+    <div class="creatures-window-form__header">
+        <h2>New Creature</h2>
+    </div>
+    <form class="creatures-window-form__body" onsubmit="submitCreatureForm(event)">
+        <label>Token
+            <input type="file">
+        </label>
+        <div class="creatures-window-form__body--box">
+            <label>Name
+                <input required onchange="creatureFormName = event.target.value">
+            </label>
+            <label>Size
+                <select onchange="creatureFormSize = event.target.value">
+                    <option value="tiny">Tiny</option>
+                    <option value="small">Small</option>
+                    <option value="medium" selected>Medium</option>
+                    <option value="large">Large</option>
+                    <option value="huge">Huge</option>
+                    <option value="gargantuan">Gargantuan</option>
+                </select>
+            </label>
+            <label>Type
+                <input class="input--md" onchange="creatureFormType = event.target.value">
+            </label>
+            <label>Alignment
+                <input class="input--sm" onchange="creatureFormAlignment = event.target.value">
+            </label>
+            <label>AC
+                <input class="input--sm" type="number" onchange="creatureFormAc = event.target.value">
+            </label>
+            <label>Hit Points
+                <input class="input--sm" type="number" onchange="creatureFormHitPoints = event.target.value">
+            </label>
+            <label>Hit Dice
+                <input class="input--sm" onchange="creatureFormHitDice = event.target.value">
+            </label>
+        </div>
+        <div class="creatures-window-form__body--box">
+            <div>
+                <div class="form__input-add form__input-add--speed">
+                    <label>Movement
+                        <div class="flex-container">
+                            <p>Walk</p>
+                            <input placeholder="30" type="number" class="input--sm creature-inputs__speed-value" required>
                         </div>
                         <div class="flex-container">
                             <p>Swim</p>
